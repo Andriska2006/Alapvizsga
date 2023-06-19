9a4a2841ae1cb4c1c60127bd42518174a5b98962
Szoszivan <60668841+Andriska2006@users.noreply.github.com>
2023-06-19 10:52:15 +0200

    Delete 20230601_Alapvizsga_2 directory

--- 20230601_Alapvizsga_2/források/3. feladat/CBadás.py
+++
@@ -1,8 +0,0 @@
-class CBadás:
-    def __init__(self, row) -> None:
-        splitted = row.split(';')
-        
-        self.Ora = int(splitted[0])
-        self.Perc = int(splitted[1])
-        self.AdasDb = int(splitted[2])
-        self.Nev = splitted[3]
